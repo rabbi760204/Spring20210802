3f7b173f42bdb502a46c76efd11faa98d1f6731b
stanf <stanf@DESKTOP-70B46R7>
2021-08-07 02:30:52 +0800

    java

--- spring/src/main/java/com/study/spring/case01/Test.java
+++ spring/src/main/java/com/study/spring/case01/Test.java
@@ -3,12 +3,13 @@ package com.study.spring.case01;
 public class Test {
 
 	public static void main(String[] args) {
-		Person person = new Person();
-		
+		//OLD
+		Person person = new Person();		
 		person.setId(1);
-		person.setName("Spring");
-		
+		person.setName("Spring");		
 		System.out.println(person);
+		//Spring		
+		
 
 	}
 
