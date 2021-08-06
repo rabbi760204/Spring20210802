8d2db96ddd8cd9e559bd72d60c05f7604d5b9a9b
stanf <stanf@DESKTOP-70B46R7>
2021-08-07 02:36:15 +0800

    java

--- spring/src/main/java/com/study/spring/case01/Test.java
+++ spring/src/main/java/com/study/spring/case01/Test.java
@@ -1,5 +1,8 @@
 package com.study.spring.case01;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -9,8 +12,9 @@ public class Test {
 		person.setName("Spring");		
 		System.out.println(person);
 		//Spring		
-		
-
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Person person2 = (Person)ctx.getBean("person");
+System.out.println(person2);
 	}
 
 }
